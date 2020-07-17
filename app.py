699ca187d66561270c1b9129af7563e9900ec243
Rana Emad <rana3emad@gmail.com>
2020-07-17 16:56:50 +0200

    show one artist

--- app.py
+++ app.py
@@ -429,7 +429,9 @@ def show_artist(artist_id):
     "past_shows_count": 0,
     "upcoming_shows_count": 3,
   }
-  data = list(filter(lambda d: d['id'] == artist_id, [data1, data2, data3]))[0]
+  # data = list(filter(lambda d: d['id'] == artist_id, [data1, data2, data3]))[0]
+  data= Artist.query.filter_by(id=artist_id).one()
+  data.genres=[]
   return render_template('pages/show_artist.html', artist=data)
 
 #  Update
