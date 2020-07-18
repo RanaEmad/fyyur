83a06b55c1d6149ac98acb0f6d096facf0dd209f
Rana Emad <rana3emad@gmail.com>
2020-07-18 17:20:11 +0200

    move models to separate file

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ from datetime import datetime
 from pprint import pprint
 from sqlalchemy import text
 import json
+from models import db, Venue, Artist, Show
 #----------------------------------------------------------------------------#
 # App Config.
 #----------------------------------------------------------------------------#
@@ -24,60 +25,11 @@ import json
 app = Flask(__name__)
 moment = Moment(app)
 app.config.from_object('config')
-db = SQLAlchemy(app)
+
+db.init_app(app)
 
 
 migrate= Migrate(app,db)
-
-#----------------------------------------------------------------------------#
-# Models.
-#----------------------------------------------------------------------------#
-
-class Venue(db.Model):
-    __tablename__ = 'Venue'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    address = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
-    genres = db.Column(db.String(500))
-
-    seeking_talent = db.Column(db.Boolean)
-    seeking_description= db.Column(db.String(120))
-    website= db.Column(db.String(120))
-    shows= db.relationship("Show",backref="Venue")
-
-class Artist(db.Model):
-    __tablename__ = 'Artist'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
-
-    website = db.Column(db.String(120))
-    seeking_venue = db.Column(db.Boolean)
-    seeking_description = db.Column(db.String(120))
-    shows= db.relationship("Show",backref="Artist")
-
-class Show(db.Model):
-    __tablename__ = 'Show'
-
-    id = db.Column(db.Integer, primary_key=True)
-    venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'))
-    artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'))
-    start_time = db.Column(db.DateTime)
-
-    venue = db.relationship("Venue", back_populates="shows")
-    artist = db.relationship("Artist", back_populates="shows")
 
 #----------------------------------------------------------------------------#
 # Filters.
